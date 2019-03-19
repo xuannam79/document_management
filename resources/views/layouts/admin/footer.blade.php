3e72d1a898938ac37cfb16dd01e2715c4db09b29
nhatquan160697 <nhatquan160697@gmail.com>
2019-03-19 11:06:09 +0700

    refactor_UI_part1

---
+++ resources/views/layouts/admin/footer.blade.php
@@ -0,0 +1,11 @@
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
+
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Sun-Asterisk 2019</span>
+        </div>
+    </div>
+</footer>
