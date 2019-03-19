aee0b1ef4f079ceadcb007d25dfd6b5484a3aaad
nhatquan160697 <nhatquan160697@gmail.com>
2019-03-19 13:26:22 +0700

    Refactory lan 2

--- resources/views/system_admin/department/index.blade.php
+++ resources/views/system_admin/department/index.blade.php
@@ -1,48 +1,56 @@
-@extends('layouts.system_admin.master')
+@extends('layouts.admin.master')
 @section('title')
     Quản lý phòng ban
 @endsection
 @section('content')
-<div class="main-content-inner">
+<div class="container-fluid">
+    <a href="{{ route('department.create') }}" class="btn btn-primary">Thêm</a>
     @include('common.errors')
-    <div class="row">
-        <!-- Dark table start -->
-        <div class="col-12 mt-5">
-            <div class="card">
-                <div class="card-body">
-                    <a href="{{ route('department.create') }}" class="btn btn-primary">Thêm</a><br><br>
-                    <div class="data-tables datatable-dark">
-                        <table id="dataTable3" class="text-center">
-                            <thead class="text-capitalize">
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Tên bộ phận</th>
-                                    <th>Ngày tạo</th>
-                                    <th>Trạng thái</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                    @foreach($departments as $department)
-
-                                <tr>
-                                    <td>{{ $department->id }}</td>
-                                    <td>{{ $department->name }}</td>
-                                    <td>{{ $department->created_at }}</td>
-                                        <td><span class="status-p bg-primary">pending</span></td>
-
-                                    <td>
-                                        <ul class="d-flex justify-content-center">
-                                            <li class="mr-3"><a href="{{ route('department.edit', $department->id) }}" class="text-secondary"><i class="fa fa-edit"></i></a></li>
-                                            <li><a href="#" class="text-danger"><i class="ti-trash"></i></a></li>
-                                        </ul>
-                                    </td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+    <br />
+    <br />
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Danh sách phòng ban</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Tên bộ phận</th>
+                            <th>Ngày tạo</th>
+                            <th>Trạng thái</th>
+                            <th>Hành động</th>
+                        </tr>
+                    </thead>
+                    <tfoot>
+                        <tr>
+                            <th>ID</th>
+                            <th>Tên bộ phận</th>
+                            <th>Ngày tạo</th>
+                            <th>Trạng thái</th>
+                            <th>Hành động</th>
+                        </tr>
+                    </tfoot>
+                    <tbody>
+                        @foreach($departments as $department)
+                        <tr>
+                            <td>{{ $department->id }}</td>
+                            <td>{{ $department->name }}</td>
+                            <td>{{ $department->created_at }}</td>
+                            <td><span class="badge badge-pill badge-warning">Warning</span></td>
+                            <td>
+                                <ul class="d-flex justify-content-center">
+                                    <li class="mr-3"><a href="{{ route('department.edit', $department->id) }}" class="text-secondary"><i class="fa fa-edit"></i></a></li>
+                                    <li><a href="#" class="text-danger"><i class="ti-trash"></i></a></li>
+                                </ul>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
