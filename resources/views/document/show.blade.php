e89768ce143c45983fa941c1e0e22273cf44e3ca
nampx-0806 <xnam7979@gmail.com>
2019-04-08 08:07:47 +0700

    update missing file

---
+++ resources/views/document/show.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.user.master')
+@section('title')
+Show
+@endsection
+@section('content')
+<div class="container">
+    <div id="cards-wrapper" class="cards-wrapper row">
+        <div style="margin: 10px">
+            <div class="detail-head">
+                <h4 style="color:black">[12367845]&nbsp;Title here</h4>
+                <br>
+                <img src="/templates/admin/img/avatar/default.png" style="width: 35px;height: 35px;border-radius: 2em;">&nbsp;
+                <span style="color: black;font-weight: bold">Pham Xuan Nam</span><span>10/04/1997</span>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
