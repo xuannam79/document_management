af8b1a7c6b6f2fd5a31638dafe919206bf841b9d
nhatquan160697 <nhatquan160697@gmail.com>
2019-04-22 10:44:14 +0700

    FE Delegacy

--- resources/views/layouts/user/header.blade.php
+++ resources/views/layouts/user/header.blade.php
@@ -13,33 +13,51 @@
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
 		<div class="collapse navbar-collapse position-relative" id="navbarNavDropdown">
 			<ul class="navbar-nav">
-				<li class="nav-item cool-link active">
-					<a class="nav-link" href="/">Trang chủ </a>
-				</li>
 				<li class="nav-item cool-link">
-					<a class="nav-link" href="#">Văn bản</a>
+					<a class="nav-link" href="/">Trang chủ </a>
 				</li>
 				<li class="nav-item cool-link dropdown">
 					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-					Dropdown
+					Văn bản
 					</a>
 					<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-						<a class="dropdown-item" href="#">Action</a>
+						<a class="dropdown-item" href="#">Văn bản đến</a>
+						<a class="dropdown-item" href="#">Văn bản đi</a>
 					</div>
 				</li>
-
+				<li class="nav-item cool-link dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Ủy quyền
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+						<a class="dropdown-item" href="{{ route('delegacy.create') }}">Thêm ủy quyền</a>
+						<a class="dropdown-item" href="{{ route('delegacy.index') }}">Danh sách ủy quyền</a>
+					</div>
+				</li>
+				<li class="nav-item cool-link">
+					<a class="nav-link" href="/">Kế hoạch tuần </a>
+				</li>
+				<li class="nav-item cool-link">
+					<a class="nav-link" href="/">Kế hoạch công tác</a>
+				</li>
+				<li class="nav-item cool-link">
+					<a class="nav-link" href="/">Hồ sơ văn bản</a>
+				</li>
+				<li class="nav-item cool-link">
+					<a class="nav-link" href="/">Biểu mẫu</a>
+				</li>
 			</ul>
 			<ul class="navbar-nav position-login">
 				@if(Auth::check())
-				<li class="nav-item cool-link dropdown">
-					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						<i class="fa fa-user"></i> {{Auth::user()->name}}
-					</a>
-					<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-						<a class="dropdown-item" href="{{ route('profile') }}">Thông tin cá nhân</a>
-						<a class="dropdown-item" href="#">Đăng xuất</a>
-					</div>
-				</li>
+					<li class="nav-item cool-link dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<i class="fa fa-user"></i> {{Auth::user()->name}}
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+							<a class="dropdown-item" href="{{ route('profile') }}">Thông tin cá nhân</a>
+							<a class="dropdown-item" href="#">Đăng xuất</a>
+						</div>
+					</li>
 				@endif
 			</ul>
 
